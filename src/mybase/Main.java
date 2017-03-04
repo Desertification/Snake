af7420cc2e67837f6db3f530c025d0635783452b
Thomas Cauwelier <thomascauwelier@gmail.com>
2017-03-04 23:04:00 +0100

    RawConsoleInput demo

--- src/mybase/Main.java
+++ src/mybase/Main.java
@@ -1,8 +1,42 @@
 package mybase;
 
+import biz.source_code.utils.RawConsoleInput;
+
+import java.io.IOException;
+
+import static java.lang.System.out;
+
+interface Drawable {
+    void draw();
+}
+
+interface Updatable {
+    void update();
+}
+
+class snake implements Drawable, Updatable {
+
+    @Override
+    public void draw() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        while (true) {
+            try {
+                out.println(RawConsoleInput.read(true));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
