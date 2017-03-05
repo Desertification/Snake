16afed87b6a3b7c24c6edc259b638a630500e91e
Thomas Cauwelier <thomascauwelier@gmail.com>
2017-03-05 01:29:22 +0100

    add point type (swing point replacement)

---
+++ src/mybase/Point.java
@@ -0,0 +1,17 @@
+package mybase;
+
+/**
+ * Created by thoma on 04-Mar-17.
+ */
+public class Point {
+    public int x, y;
+
+    public Point() {
+        this(0, 0);
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
