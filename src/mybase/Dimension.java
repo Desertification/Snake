9ac94770cde215572df03da22f81f0d8e3c8e228
Thomas Cauwelier <thomascauwelier@gmail.com>
2017-03-08 01:41:47 +0100

    add dimension type

---
+++ src/mybase/Dimension.java
@@ -0,0 +1,14 @@
+package mybase;
+
+/**
+ * Created by thoma on 08-Mar-17.
+ */
+public class Dimension extends Point {
+    public Dimension() {
+        super();
+    }
+
+    public Dimension(int x, int y) {
+        super(x, y);
+    }
+}
