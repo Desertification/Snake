0857fa1668213ffdb433a23e5d88cf75f736938f
Thomas Cauwelier <thomascauwelier@gmail.com>
2017-03-11 19:34:33 +0100

    test noise

--- src/mybase/ConsoleBenchmark.java
+++ src/mybase/ConsoleBenchmark.java
@@ -1,12 +1,16 @@
 package mybase;
 
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class ConsoleBenchmark {
+    private static String[] chars = {"\u2588", "\u00DE", "\u25a0", "\u2591", "\u2592", "\u2593"};
+    private static Random random = new Random();
+
     public static void main(String[] args) {
         Timer timer = new Timer(false);
-        timer.scheduleAtFixedRate(new FrameTask(), 0, 100);
+        timer.scheduleAtFixedRate(new FrameTask(), 0, 40);
     }
 
     static class FrameTask extends TimerTask {
@@ -20,7 +24,7 @@ public class ConsoleBenchmark {
             ConsoleFrame frame = new ConsoleFrame(new Dimension(size_x, size_y));
             for (int y = 0; y < size_y; y++) {
                 for (int x = 0; x < size_x; x++) {
-                    frame.setCell(new Point(x, y), "\u2588");
+                    frame.setCell(new Point(x, y), chars[random.nextInt(chars.length)]);
                 }
             }
             long fg_stop = System.currentTimeMillis();
