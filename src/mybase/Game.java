a459f65d2d330e635f2d7524034b2395401284e0
Thomas Cauwelier <thomascauwelier@gmail.com>
2017-03-11 23:56:10 +0100

    add game class to store global game related stuff

---
+++ src/mybase/Game.java
@@ -0,0 +1,17 @@
+package mybase;
+
+/**
+ * Created by thoma on 11-Mar-17.
+ */
+public class Game {
+    private Dimension bounds;
+
+
+    public Dimension getBounds() {
+        return bounds;
+    }
+
+    public void setBounds(Dimension bounds) {
+        this.bounds = bounds;
+    }
+}
