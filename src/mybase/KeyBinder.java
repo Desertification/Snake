9c3597ade641c04554b62ff398b914f57f0aa521
Thomas Cauwelier <thomascauwelier@gmail.com>
2017-03-15 01:34:31 +0100

    add KeyBinder class (observer and command pattern)

---
+++ src/mybase/KeyBinder.java
@@ -0,0 +1,39 @@
+package mybase;
+
+import myLinkedList.Iterator;
+import myLinkedList.LinkedList;
+
+/**
+ * Created by thoma on 15-Mar-17.
+ */
+public class KeyBinder implements KeyObserver {
+    private final int key;
+    private LinkedList<Command> commands;
+
+    public KeyBinder(int key) {
+        this.key = key;
+        commands = new LinkedList<>();
+    }
+
+    public synchronized void addCommand(Command command) {
+        commands.append(command);
+    }
+
+    public synchronized void removeCommand(Command command) {
+        commands.remove(command);
+    }
+
+    private void executeCommands() {
+        Iterator<Command> iterator = commands.iterator();
+        while (iterator.hasNext()) {
+            iterator.next().execute();
+        }
+    }
+
+    @Override
+    public void update(Integer update) {
+        if (update == key) {
+            executeCommands();
+        }
+    }
+}
