fad4ed12bbfb7c2307d14baf109f7245aa643c16
Thomas Cauwelier <thomascauwelier@gmail.com>
2017-03-15 01:35:23 +0100

    add KeyBinding lookup utility class

---
+++ src/mybase/KeyBindings.java
@@ -0,0 +1,13 @@
+package mybase;
+
+/**
+ * Created by thoma on 12-Mar-17.
+ */
+public abstract class KeyBindings {
+    public static int up = 57416;
+    public static int down = 57424;
+    public static int left = 57419;
+    public static int right = 57421;
+    public static int esc = 27;
+    public static int enter = 13;
+}
