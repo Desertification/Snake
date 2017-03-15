ef14b09d710e928d59cf0de866c19f4e0d75e09d
Thomas Cauwelier <thomascauwelier@gmail.com>
2017-03-15 01:35:53 +0100

    add method to find the opposite

--- src/mybase/Direction.java
+++ src/mybase/Direction.java
@@ -4,5 +4,19 @@ package mybase;
  * Created by thoma on 04-Mar-17.
  */
 enum Direction {
-    Up, Down, Left, Right
+    UP, DOWN, LEFT, RIGHT;
+
+    static {
+        UP.opposite = DOWN;
+        DOWN.opposite = UP;
+        LEFT.opposite = RIGHT;
+        RIGHT.opposite = LEFT;
+    }
+
+    // http://stackoverflow.com/a/18883717
+    private Direction opposite;
+
+    public Direction opposite() {
+        return opposite;
+    }
 }
