d38a2bca9703d224c157a0fe127a7fc46ce2b1db
Thomas Cauwelier <thomascauwelier@gmail.com>
2017-03-16 23:32:52 +0100

    implement collidable, add collideBehavior field and setter

--- src/mybase/Snake.java
+++ src/mybase/Snake.java
@@ -2,15 +2,18 @@ package mybase;
 
 import myLinkedList.LinkedList;
 
-class Snake implements Drawable, Updatable {
+class Snake implements Drawable, Updatable, Collidable {
     private Updatable updateBehavior;
     private Drawable drawBehavior;
     private LinkedList<Location> body;
     private Direction direction;
+    private CollideBehavior collideBehavior;
+    private Hitbox hitbox;
 
     Snake() {
         body = new LinkedList<>(new Location(0, 0));
         direction = Direction.RIGHT;
+        hitbox = new SnakeHitbox(body);
     }
 
     public LinkedList<Location> getBody() {
@@ -47,5 +50,24 @@ class Snake implements Drawable, Updatable {
 
     public void setDrawBehavior(Drawable drawBehavior) {
         this.drawBehavior = drawBehavior;
+    }
+
+    public void setCollideBehavior(CollideBehavior collideBehavior) {
+        this.collideBehavior = collideBehavior;
+    }
+
+    @Override
+    public void collideWith(Collidable other) {
+        collideBehavior.collideWith(other);
+    }
+
+    @Override
+    public void collideWith(Snake snake) {
+        collideBehavior.collideWith(snake);
+    }
+
+    @Override
+    public Hitbox getHitbox() {
+        return hitbox;
     }
 }
