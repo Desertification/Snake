6be2f4a4ad816c47e1bc7fb6f449a7f6fcd88cd9
Thomas Cauwelier <thomascauwelier@gmail.com>
2017-03-19 03:02:57 +0100

    implementing code logic from Main.java

--- src/mybase/Game.java
+++ src/mybase/Game.java
@@ -1,10 +1,29 @@
 package mybase;
 
+import myLinkedList.Iterator;
+import myLinkedList.LinkedList;
+
 /**
  * Created by thoma on 11-Mar-17.
  */
-public class Game {
+public class Game implements Runnable {
     private Dimension bounds;
+    private LinkedList<Entity> entities;
+    private LinkedList<Entity> players;
+    private ConsoleFrameDrawer frameDrawer;
+    private EntityCreator entityCreator;
+    private int targetDeltaTime = 100;
+
+    public Game() {
+        entities = new LinkedList<>();
+        bounds = new Dimension(30, 15);
+        frameDrawer = new ConsoleFrameDrawer(System.out, bounds);
+        entityCreator = new EntityCreator(this);
+    }
+
+    public ConsoleFrameDrawer getFrameDrawer() {
+        return frameDrawer;
+    }
 
     public Dimension getBounds() {
         return bounds;
@@ -14,7 +33,86 @@ public class Game {
         this.bounds = bounds;
     }
 
+    public void loadLevel() {
+        // todo implement level object (matrix of entitytypes?)
+        bounds = new Dimension(30, 15); // todo get from level
+        entities = new LinkedList<>();
+        frameDrawer = new ConsoleFrameDrawer(System.out, bounds);
+        // todo actual loading
+    }
+
+    public void spawnEntity(EntityType entity, Point location, Direction direction) {
+        //todo create with entity creator
+    }
+
+    public void spawnPlayer(EntityType entity, Point location, Direction direction) {
+        spawnEntity(entity, location, direction);
+        // todo keybind
+    }
+
+    protected boolean isEntityAPlayer(Entity entity) {
+        Iterator<Entity> iterator = players.iterator();
+        while (iterator.hasNext()) {
+            Entity next = iterator.next();
+            if (next == entity) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    protected boolean isGameOver() {
+        return players.isEmpty();
+    }
+
+    public void deleteEntity(Entity entity) {
+        if (isEntityAPlayer(entity)) {
+            players.remove(entity);
+            //todo undo keybinding
+        }
+        entities.remove(entity);
+    }
+
+    public void handleCollision(Entity entity) {
+        Hitbox entityHitbox = entity.getHitbox();
+        Iterator<Entity> iterator = entities.iterator();
+        while (iterator.hasNext()) {
+            Entity next = iterator.next();
+            boolean collidingWith = entityHitbox.isCollidingWith(next.getHitbox());
+            if (collidingWith) {
+                entity.collideWith(next); // todo this is not going to work as expected :(
+                //see http://stackoverflow.com/questions/36722057/polymorphism-doesnt-work-in-method-arguments-in-java
+                // FIX WIP src/experimental/VisitorDemo.java
+            }
+        }
+    }
+
     public void exit() {
         System.exit(0);
+    }
+
+    @Override
+    public void run() { // todo needs refactoring
+        while (true) {
+            long begin = System.nanoTime();
+
+            Iterator<Entity> iterator = entities.iterator();
+            while (iterator.hasNext()) {
+                Entity entity = iterator.next();
+                entity.move();
+                handleCollision(entity);
+                entity.draw();
+            }
+            // todo is game over
+            frameDrawer.drawFrame();
+            frameDrawer.clearFrame();
+
+            long delta = (System.nanoTime() - begin) / 1000000;
+            try {
+                Thread.sleep(Math.max(0, delta));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
