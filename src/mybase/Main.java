f49ce799209598568077e2f175fbfa652609c209
Thomas Cauwelier <thomascauwelier@gmail.com>
2017-03-19 23:09:40 +0100

    add walls and one piece of food

--- src/mybase/Main.java
+++ src/mybase/Main.java
@@ -10,7 +10,20 @@ package mybase;
 public class Main {
     public static void main(String[] args) {
         Game game = new Game();
-        game.spawnPlayer(EntityType.SNAKE, new Point(5, 5), Direction.RIGHT);
+        Dimension bounds = game.getBounds();
+        game.spawnPlayer(EntityType.SNAKE, new Point(5, bounds.height / 2), Direction.RIGHT);
+        game.spawnEntity(EntityType.FOOD);
+        for (int x = 0; x < bounds.width; x++) {
+            game.spawnEntity(EntityType.WALL, new Point(x, 0));
+            game.spawnEntity(EntityType.WALL, new Point(x, bounds.height - 1));
+        }
+        for (int y = 1; y < bounds.height - 1; y++) {
+            if (y == bounds.height / 2) {
+                continue;
+            }
+            game.spawnEntity(EntityType.WALL, new Point(0, y));
+            game.spawnEntity(EntityType.WALL, new Point(bounds.width - 1, y));
+        }
         game.run();
     }
 }
