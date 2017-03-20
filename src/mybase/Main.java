f9092b8db15c6fc444b53a695a92828cefab296c
Thomas Cauwelier <thomascauwelier@gmail.com>
2017-03-20 01:20:14 +0100

    remove some todos

--- src/mybase/Main.java
+++ src/mybase/Main.java
@@ -1,9 +1,6 @@
 package mybase;
 
 //todo bug, you can put the snake in reverse if fast enough
-//todo BUILDERS, the game can use some builders
-//todo food for the poor snake
-//todo collide code
 //todo hide linked list implementation in snake
 //todo MORE tests
 
